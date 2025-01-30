3e04d9e60b4e58254924d1f8d32446b4000f776d
CDonwon <ichivasov560@gmail.com>
2025-01-30 21:05:07 +0300

    Интерфейс для изменения параметров

--- LoRaReceiver/LoRaReceiver.ino
+++ LoRaReceiver/LoRaReceiver.ino
@@ -1,6 +1,18 @@
 // Only supports SX1276/SX1278
+#include <WiFi.h>
+#include <WebServer.h>
+#include <HTTPClient.h>
 #include <LoRa.h>
 #include "LoRaBoards.h"
+
+
+const char* ssid = "LilBoPeevo";  
+const char* password = "123123124";
+
+
+String webMessage = "Waiting for update...";
+
+WebServer server(80);
 
 #ifndef CONFIG_RADIO_FREQ
 #define CONFIG_RADIO_FREQ           868.0
@@ -17,15 +29,78 @@
 #error "LoRa example is only allowed to run SX1276/78. For other RF models, please run examples/RadioLibExamples
 #endif
 
-int packetsNums = 100;
-int errorPack = 0;
-int packetsCounter = 0;
-unsigned long lastPacketTime = 0;
-const unsigned long timeout = 600;
+int spreadingFactor = 12;
+int txPower = CONFIG_RADIO_OUTPUT_POWER;
+float signalBandwidth = CONFIG_RADIO_BW;
+
+
+void applyLoRaSettings() {
+    LoRa.setSpreadingFactor(spreadingFactor);
+    LoRa.setTxPower(txPower);
+    LoRa.setSignalBandwidth(signalBandwidth * 1000);
+}
 
 void setup()
 {
     setupBoards();
+    WiFi.begin(ssid, password);
+    Serial.begin(115200);
+    Serial.print("Connecting to WiFi...");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("\nConnected to Wi-Fi!");
+    Serial.print("ESP32 Local IP: ");
+    Serial.println(WiFi.localIP());
+    
+
+    server.on("/", HTTP_GET, []() {
+        String page = "<html>\
+        <body>\
+        <h2>LoRa Web Interface</h2>\
+        <form action='/send' method='POST'>\
+        <input type='text' name='message' maxlength='20'>\
+        <input type='submit' value='Send'>\
+        </form>\
+        <h3>LoRa Settings</h3>\
+        <form action='/update' method='POST'>\
+        Spreading Factor (7-12): <input type='number' name='sf' min='7' max='12' value='" + String(spreadingFactor) + "'><br>\
+        Tx Power (2-20 dBm): <input type='number' name='tx' min='2' max='20' value='" + String(txPower) + "'><br>\
+        Signal Bandwidth (7.8-500 kHz): <input type='number' name='bw' step='0.1' min='7.8' max='500' value='" + String(signalBandwidth) + "'><br>\
+        <input type='submit' value='Update'>\
+        </form>\
+        </body>\
+        </html>";
+        server.send(200, "text/html", page);
+    });
+
+
+    server.on("/update", HTTP_POST, []() {
+        if (server.hasArg("sf") && server.hasArg("tx") && server.hasArg("bw")) {
+            int newSF = server.arg("sf").toInt();
+            int newTx = server.arg("tx").toInt();
+            float newBW = server.arg("bw").toFloat();
+
+            if (newSF >= 7 && newSF <= 12) spreadingFactor = newSF;
+            if (newTx >= 2 && newTx <= 20) txPower = newTx;
+            if (newBW >= 7.8 && newBW <= 500) signalBandwidth = newBW;
+
+            applyLoRaSettings();
+            Serial.println("LoRa settings updated:");
+            Serial.print("Spreading Factor: "); Serial.println(spreadingFactor);
+            Serial.print("Tx Power: "); Serial.println(txPower);
+            Serial.print("Signal Bandwidth: "); Serial.println(signalBandwidth);
+        }
+        server.sendHeader("Location", "/");
+        server.send(303);
+    });
+
+    server.begin();
+    Serial.println("HTTP Server Started");
+
+    
     // When the power is turned on, a delay is required.
     delay(1500);
 
@@ -42,11 +117,7 @@ void setup()
         while (1);
     }
 
-    LoRa.setTxPower(CONFIG_RADIO_OUTPUT_POWER);
-
-    LoRa.setSignalBandwidth(CONFIG_RADIO_BW * 1000);
-
-    LoRa.setSpreadingFactor(12);
+    applyLoRaSettings();
 
     LoRa.setPreambleLength(16);
 
@@ -58,34 +129,19 @@ void setup()
 
     LoRa.setCodingRate4(7);
 
-    // put the radio into receive mode
     LoRa.receive();
 
 }
 
 void loop()
 {
-
-    // try to parse packet
+    server.handleClient();
     int packetSize = LoRa.parsePacket();
     if (packetSize) {
-        // received a packet
-        lastPacketTime = millis();
-        //Serial.print("Received packet '");
-
         String recv = "";
-        // read packet
         while (LoRa.available()) {
             recv += (char)LoRa.read();
         }
-        if(recv.substring(0, 5) != "hello"){
-          errorPack++;
-        }
-
-        // Serial.println(recv);
-
-        // print RSSI of packet
-        // Serial.print("' with RSSI ");
         Serial.print(LoRa.packetRssi());
         Serial.print(" ");
         Serial.print(LoRa.packetSnr());
@@ -101,24 +157,5 @@ void loop()
             u8g2->drawStr(0, 56, buf);
             u8g2->sendBuffer();
         }
-        packetsCounter++;
-        packetsNums--;
     }
-    // else{
-    //   if (millis() - lastPacketTime > timeout){
-    //      Serial.println("No packets received for 0.5 seconds!");
-    //      packetsNums--;
-    //      lastPacketTime = millis();
-    //   }
-    // }
-    
-    // if (packetsNums == 0){
-    //   Serial.println("total packets: ");
-    //   Serial.println(packetsCounter);
-    //   Serial.println("Error packets: ");
-    //   Serial.println(errorPack);
-    //   packetsNums = 100;
-    //   errorPack = 0;
-    //   packetsCounter = 0;
-    // }
 }
